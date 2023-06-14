f1001c9235ea3914702d0660835699d49c420251
David Hernández <josedavid_0727@outlook.com>
2023-06-13 23:32:55 -0500

    median filter issue solved

--- src/algorithms/denoising/medianFilter.py
+++ src/algorithms/denoising/medianFilter.py
@@ -3,17 +3,14 @@ from src.algorithms.neighbors import get_neighbors
 
 
 def median_filter(image):
-    rows_image = image.shape[0]
-    cols_image = image.shape[1]
-    sum_values = 0
     filtered_image = np.zeros_like(image)
-    for row in range(rows_image):
-        for col in range(cols_image):
-            neighbors = get_neighbors(image, row, col, 1, 0, image.shape[2]-1)
-            for neighbor in neighbors:
-                x_index = neighbor[0]
-                y_index = neighbor[1]
-                sum_values += image[x_index, y_index]
-            filtered_image[row, col] = np.median(neighbors)
-            sum_values = 0
+    for x in range(1, image.shape[0] - 2):
+        for y in range(1, image.shape[1] - 2):
+            for z in range(1, image.shape[2] - 2):
+                avg = 0
+                for dx in range(-1, 1):
+                    for dy in range(-1, 1):
+                        for dz in range(-1, 1):
+                            avg = avg + image[x + dx, y + dy, z + dz]
+                filtered_image[x + 1, y + 1, z + 1] = avg / 27
     return filtered_image
